6576d1a82f3fa6d824af64a66fce04b591c0445a
Jean Dubois <jd-dev@laposte.net>
2022-06-24 15:42:00 +0200

    4.1

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 # coding:utf-8
-# Développé par Jean Dubois
-# Générateur aléatoire de personnes
+
+# Générateur de personnes / Identity Randomizer : a program that randomly generates fake people
+# Copyright (C) 2020-2022 Jean Dubois
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from tkinter import *  # Tkinter sert à faire des fenêtres
 from tkinter import ttk  # Pour faire les onglets
@@ -16,7 +30,7 @@ import random  # Pour pouvoir faire du pseudo-aléatoire
 from random import randint  # Pour pas avoir à écrire à chaque fois 'random.randint()'
 
 __author__ = "Jean Dubois <jd-dev@laposte.net>"
-__version__ = "22w25a"
+__version__ = "4.1"
 LG = "lightgreen"
 PG = "palegreen"
 CCEEFF = '#CCEEFF'
